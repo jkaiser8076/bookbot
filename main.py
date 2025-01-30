e4c9e1061447d321f210a4e8acc92db9a7d6008f
jkaiser8076 <jkaiser8076@gmail.com>
2025-01-30 11:36:29 -0500

    Listed alphanumerical characters by occurance

--- main.py
+++ main.py
@@ -1,8 +1,39 @@
 def main():
-	with open("books/frankenstein.txt") as f:
-		file_contents = f.read()
-		print(file_contents)
-	words = file_contents.split()
-	print(f"Total word count is {len(words)}.")
+	book_path = "books/frankenstein.txt"
+	text = get_book_text(book_path)
+	wordcount = get_wordcount(text)
+	chars_dict = get_chars_dict(text)
+	sorted_list = sort_on(text)
+	for entry in sorted_list:
+		print(f"The '{entry[0]}' character was found {entry[1]} times.")
+
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+   
+
+def get_wordcount(text):
+	words = text.split()
+	return len(words)
+
+
+def get_chars_dict(text):
+	chars = {}
+	for char in text:
+		if char.isalpha():
+			lowered = char.lower()
+			if lowered in chars:
+				chars[lowered] += 1
+			else:
+				chars[lowered] = 1
+	return chars
+
+
+def sort_on(text):
+	chars = get_chars_dict(text)
+	sorted_list = sorted(chars.items(), key=lambda x: x[1], reverse=True)
+	return sorted_list
+
 
 main()
